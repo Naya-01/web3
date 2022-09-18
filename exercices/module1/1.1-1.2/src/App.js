0e6fce5abe06208a6e2bc4a021fba56c1df5b6b6
rayan abarkan <rayan.abarkan@student.vinci.be>
2022-09-18 12:38:28 +0200

    1.1-1.2 : refactor - absolute imports

--- exercices/module1/1.1-1.2/src/App.js
+++ exercices/module1/1.1-1.2/src/App.js
@@ -1,6 +1,6 @@
-import Header from "./components/Header"
-import Content from "./components/Content"
-import Total from "./components/Total"
+import Content from "components/Content"
+import Header from "components/Header"
+import Total from "components/Total"
 
 const App = () => {
   const course = 'Half Stack application development'
