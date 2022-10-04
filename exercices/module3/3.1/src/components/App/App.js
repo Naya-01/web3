6cbc4307f7305609a337b5fab0b9c46cff4b6207
rayan abarkan <rayan.abarkan@student.vinci.be>
2022-10-04 11:54:37 +0200

    module3 : basics

---
+++ exercices/module3/3.1/src/components/App/App.js
@@ -0,0 +1,39 @@
+import Button from 'components/Button/Button'
+import Statistics from 'components/Statistics/Statistics'
+import { useState } from 'react'
+
+
+
+// a proper place to define a component
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClickGood = () =>{
+    setGood(good+1);
+  }
+
+  const handleClickNeutral = () =>{
+    setNeutral(neutral+1);
+  }
+
+  const handleClickBad = () =>{
+    setBad(bad+1);
+  }
+  return (
+    <div>
+      <h2>Give feedback</h2>
+      <Button handleClick={handleClickGood} text="good"/>
+      <Button handleClick={handleClickNeutral} text="neutral"/>
+      <Button handleClick={handleClickBad} text="bad"/>
+
+      <Statistics good={good} neutral={neutral} bad={bad}  />
+    </div>
+  )
+}
+
+export default App
